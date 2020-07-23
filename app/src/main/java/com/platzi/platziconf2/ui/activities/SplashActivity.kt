3aeaa129ae9188157bd12c0c1ad4420053276fa3
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-07-23 11:00:31 -0500

    Diseño de splash activity realizado

---
+++ app/src/main/java/com/platzi/platziconf2/ui/activities/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.platzi.platziconf2.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platzi.platziconf2.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
