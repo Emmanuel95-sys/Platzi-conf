c007be7e2b66cf54246245617485354ccc8984fe
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-07-24 16:26:50 -0500

    Agregando marcador

--- app/src/main/java/com/platzi/platziconf2/view/ui/fragments/LocationFragment.kt
+++ app/src/main/java/com/platzi/platziconf2/view/ui/fragments/LocationFragment.kt
@@ -1,15 +1,20 @@
 package com.platzi.platziconf2.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import com.platzi.platziconf2.R
 import com.platzi.platziconf2.model.Location
@@ -32,6 +37,19 @@ class LocationFragment : Fragment(), OnMapReadyCallback {
         val zoom = 16f
         val centerMap = LatLng(location.latitud, location.longitud)
         googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap,zoom))
+        //agregando marcador
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMap)
+        markerOptions.title("Platzi Conf 2019")
+        val bitMapLogoPlatzi = context?.applicationContext?.let {
+            ContextCompat.getDrawable(it, R.drawable.logo_platzi)
+        } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitMapLogoPlatzi.bitmap, 150,150,false)
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        //agregar marcardor a mapa
+        googleMap?.addMarker(markerOptions)
+
     }
 
 }
