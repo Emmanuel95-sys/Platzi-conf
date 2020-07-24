e340b6700566c70ccca89d8675279ae2644b5a50
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-07-24 16:18:08 -0500

    Correccion de fragmentso de ubicacion, agregando zoom y centrado

--- app/src/main/java/com/platzi/platziconf2/model/Location.kt
+++ app/src/main/java/com/platzi/platziconf2/model/Location.kt
@@ -1,11 +1,11 @@
 package com.platzi.platziconf2.model
 
-class Location {
-    var name = ""
-    var address = ""
-    var latitude = ""
-    var longitude = ""
-    var phone = ""
-    var website = ""
-    var urlPhoto = ""
+class Location{
+    val name = "Centro Cultural Roberto Cantoral"
+    val address = "Puente Xoco s/n, Xoco, 03330 Ciudad de México, CDMX"
+    val latitud = 19.362028
+    val longitud = -99.166414
+    val phone = "+52 55 1519 4670"
+    val website = "https://elcantoral.com"
+    val photo = "https://lh5.googleusercontent.com/p/AF1QipPj-rZ_uZmMzvQT3iVlA832hei26T_qjFbjYiW6=w408-h270-k-no"
 }
